7f5cd85cd5372ac3c32f4a84d88448c9213196f0
정핸모 <gusah009@naver.com>
2023-01-14 18:00:52 +0900

    feat: main 로직 생성

--- src/main/kotlin/menu/Application.kt
+++ src/main/kotlin/menu/Application.kt
@@ -1,5 +1,22 @@
 package menu
 
+import menu.domain.Categories
+import menu.domain.Category
+import menu.logic.CategoryRecommend
+import menu.logic.recommendMenu
+import menu.view.inputCoaches
+import menu.view.outputResult
+import menu.view.outputStart
+
 fun main() {
-    // TODO: 프로그램 구현
+    val coaches = inputCoaches()
+    val categoryRecommend = CategoryRecommend(2)
+    val categories = mutableListOf<Category>()
+    (1..5).forEach {
+        val category = categoryRecommend.getRecommendedCategory(null)
+        recommendMenu(category, coaches)
+        categories.add(category)
+    }
+    outputStart()
+    outputResult(Categories(categories), coaches)
 }
